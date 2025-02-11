02bb0a447a16506eb467e79052760583725b2854
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2025-02-10 17:37:48 -0800

    Create player.py

---
+++ player.py
@@ -0,0 +1,28 @@
+from ollama import chat
+from ollama import ChatResponse
+import re
+
+
+class Player:
+    def __init__(self, modelo, chat=[]):
+        self.modelo = modelo
+        self.chat = chat
+
+    def borrar_pensamiento(self, texto):
+        patron = r'<think>.*?</think>'
+        texto_limpio = re.sub(patron, '', texto, flags=re.DOTALL)
+        return texto_limpio
+
+    def limpiar_respuesta(self, texto):
+        texto_limpio = self.borrar_pensamiento(texto)
+        return f"{str(self)}: {texto_limpio.strip()}"
+
+    def actualizar_chat(self, texto, sender):
+        self.chat.append({'role': sender, 'content': texto})
+
+    def generar_respuesta(self):
+        response: ChatResponse = chat(model=self.modelo, messages=self.chat)
+        return self.limpiar_respuesta(response.message.content)
+
+    def __str__(self):
+        return f'{self.modelo}'
